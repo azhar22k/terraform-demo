b454d2ef1bc4ab42068e6fb12ca71d6b0a8ca918
azhar22k <azhar.khan.78601@gmail.com>
2020-05-19 12:20:30 +0530

    Added k8s provider

--- my-infra.tf
+++ my-infra.tf
@@ -17,15 +17,19 @@ variable "stage" {
         error_message = "Invalid stage! Allowed values are [latest, test, beta, prod]."
     }
 }
-variable "bucket_postfix" {
-    description = "Generated Buckename will {stage}-demo{postfix}"
+variable "tenant" {
+    description = "Generated resources will {stage}-demo-{tenant}"
     type = string
-    default = ""
 }
 provider "aws" {
     version = "~> 2.62"
     profile = "peak-dev"
     region = var.region
+}
+
+provider "kubernetes" {
+    version = "~> 1.11"
+    config_context = "dev"
 }
 
 locals {
@@ -36,13 +40,24 @@ locals {
         service = "demo"
     }
 }
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.stage}-demo${var.bucket_postfix}"
+resource "aws_s3_bucket" "tenant_bucket" {
+  bucket = "${var.stage}-demo-${var.tenant}"
   acl    = "private"
   tags = local.tags
 }
 
+resource "kubernetes_namespace" "tenant_namespace" {
+  metadata {
+    labels = local.tags
+    name = "${var.stage}-demo-${var.tenant}"
+  }
+}
+
 output "s3_arn" {
-    value = aws_s3_bucket.bucket.arn
+    value = aws_s3_bucket.tenant_bucket.arn
     description = "ARN of S3 bucket"
+}
+
+output "ns_uid" {
+    value = kubernetes_namespace.tenant_namespace.id
 }
